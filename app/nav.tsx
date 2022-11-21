d93f87f6dfaf9eef9d5aa5b0ddb305269cb1dda5
Ahkal <bp@sojourn.llc>
2022-11-21 13:45:19 -0500

    Updates logo from svg to component

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,19 +1,11 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import Logo from "./logo";
 
 const Nav = () => {
-  const [color, setColor] = useState('white');
-  useEffect(() => {
-    const element = document.querySelector('body')
-    const style = getComputedStyle(element)
-    setColor(style.color);
-  }, []);
-
   return (
     <nav className="">
       <Link href="/">
-        <Logo fill={color} />
+        <Logo />
       </Link>
     </nav>
   );
